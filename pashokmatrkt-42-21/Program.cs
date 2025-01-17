07447e1688572105dad54d4482bdbcbf6068d55f
pasha-matrenin-kt-42-21 <as.assa.as2@mail.ru>
2025-01-17 16:35:11 +0300

    add logging and weatherForecast test request

--- pashokmatrkt-42-21/Program.cs
+++ pashokmatrkt-42-21/Program.cs
@@ -1,23 +1,40 @@
+
+using NLog;
+using NLog.Web;
+
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    logger.Error(ex, "Stoped program because of exception");
+}
+finally
+{
+    LogManager.Shutdown();
 }
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
